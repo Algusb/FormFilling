3dc167becec9ab74e2da02349c396f0b36a7465d
Aleks Gusev <algusb@mail.ru>
2022-08-05 00:07:27 +0300

    create Post Main Formdate

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Post post = new Post();
+        post.name = "Иван";
+        post.passport = "4444 № 44444444";
+        post.patronymic = "Иванович";
+        post.phone = "+7 (999)-999-99-99";
+        post.surname = "Иванов";
+        post.subscription = true;
+
+        FormDate date = new FormDate();
+        date.day = 13;
+        date.month = 10;
+        date.year = 1990;
+    }
+}
